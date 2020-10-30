84b3c2cc1b24879f09ac412f0c8e417134f583b7
yfmars-dev <72428105+yfmars-dev@users.noreply.github.com>
2020-10-30 10:46:22 +0700

    Update MarsFactory.spec.ts

--- test/MarsFactory.spec.ts
+++ test/MarsFactory.spec.ts
@@ -68,7 +68,7 @@ describe('MarsFactory', () => {
   it('createPair:gas', async () => {
     const tx = await factory.createPair(...TEST_ADDRESSES)
     const receipt = await tx.wait()
-    expect(receipt.gasUsed).to.eq(2509120)
+    expect(receipt.gasUsed).to.eq(2502886)
   })
 
   it('setFeeTo', async () => {
